aadef95a3b886f9c1ae71a1f291f768f80a81ecc
Rafa3lP <rafaelprenholato@hotmail.com>
2022-03-17 23:24:39 -0300

    começo imagem

--- src/main/java/br/ufes/ufespic/presenter/Application.java
+++ src/main/java/br/ufes/ufespic/presenter/Application.java
@@ -8,12 +8,17 @@ import br.ufes.ufespic.logger.adapter.LoggerAdapter;
 import br.ufes.ufespic.logger.adapter.LoggerAdapterFactory;
 import br.ufes.ufespic.session.Session;
 import br.ufes.ufespic.util.PropertyManager;
+import br.ufes.ufespic.view.AplicarFiltroView;
+import com.pss.imagem.processamento.decorator.AzulDecorator;
 import com.pss.imagem.processamento.decorator.Imagem;
 import com.pss.imagem.processamento.decorator.ImagemComponente;
 import com.pss.imagem.processamento.decorator.PixeladaDecorator;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.ImageIcon;
 import javax.swing.SwingUtilities;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -27,6 +32,10 @@ public class Application {
     private static String logFormat;
     private static PropertyManager propertyManager;
     
+    private static ImagemComponente imagem;
+    private static ImagemComponente imagemOld;
+    private static AplicarFiltroView view;
+    
     public Application() {
         session = Session.getInstancia();
         propertyManager = new PropertyManager();
@@ -36,9 +45,26 @@ public class Application {
         logger = new LoggerAdapterFactory().cria(logFormat);
         
         try {
-            ImagemComponente imagem = new Imagem("imagens/boys-fun.jpg");
+            imagem = new Imagem("imagens/boys-fun.jpg");
+            view = new AplicarFiltroView();
+            view.getBtnFechar().addActionListener((e) -> {
+                view.dispose();
+            });
+            view.getChkImagemAzul().addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(view.getChkImagemAzul().isSelected()) {
+                        change();
+                    } else {
+                        restore();
+                    }
+                }
+            });
+            view.getLblImagem().setIcon(new ImageIcon(imagem.getImagem()));
+            new MainPresenter().addToDesktopPane(view);
+            view.setVisible(true);
             //imagem = new PixeladaDecorator(imagem, 5);
-            imagem.visualizar();
+            //imagem.visualizar();
         } catch (InterruptedException | IOException ex) {
             System.out.println("Erro: " + ex.getMessage());
             ex.printStackTrace();
@@ -46,6 +72,33 @@ public class Application {
         
         
         //new MainPresenter();
+    }
+    
+    public static void change() {
+        try {
+            imagemOld = imagem;
+            imagem = new AzulDecorator(imagem);
+            view.getLblImagem().setIcon(new ImageIcon(imagem.getImagem()));
+            view.repaint();
+            view.setVisible(true);
+
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void restore() {
+        try {
+            imagem = imagemOld;
+            view.getLblImagem().setIcon(new ImageIcon(imagem.getImagem()));
+            view.repaint();
+            view.setVisible(true);
+
+        } catch (IOException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public static String getLogFormat() {
